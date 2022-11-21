c456158c6745fe8485201aff41abac25faf3a733
haerisogyumo <haerisogyumo@gmail.com>
2022-11-21 13:44:46 +0900

    2022/11/21

---
+++ JBoard2/src/main/java/kr/co/jboard2/controller/user/CheckNickController.java
@@ -0,0 +1,9 @@
+package kr.co.jboard2.controller.user;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/user/checkNick.do")
+public class CheckNickController extends HttpServlet{
+
+}
